0c29291cee790391d740106d5f959d8a158ae8f1
Julien Beghain <julienbeghain@gmail.com>
2023-11-01 20:18:39 +0000

    27:48 Clerk works

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,8 @@
-
+import { UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
-    <main>
-      <h1>Threads</h1>
-    </main>
-  );
+    <div>
+      <UserButton afterSignOutUrl="/"/>
+    </div>
+  )
 }
