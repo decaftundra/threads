ad970b2ab9bda77644917b1ee5fd24f7083fef1f
Julien Beghain <julienbeghain@gmail.com>
2023-11-02 15:38:34 +0000

    46:19 Topbar Started - Doing LeftSide

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,5 +1,15 @@
+import { sidebarLinks } from "@/constants";
 function LeftSidebar() {
-  return <h1>LeftSidebar</h1>;
+  return (
+    <section className="custom-scrollbar leftsidebar">
+      <div className="flex w-full flex-1 flex-col gap-6 px-6"></div>
+      {sidebarLinks.map((link) => (
+        <div>
+          Link
+        </div>
+      ))}
+    </section>
+  );
 }
 
 export default LeftSidebar;
