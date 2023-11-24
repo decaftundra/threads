c2eaaf149ea60c70f639bc3bb5dfa8fc74276569
Julien Beghain <julienbeghain@gmail.com>
2023-11-24 22:51:07 +0000

    3:13:10 Created threadCard component

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -32,3 +32,31 @@ export async function createThread({
     throw new Error(`Error creating thread: ${error.message}`);
   }
 }
+
+export async function fetchPosts(pageNumber = 1, pageSize = 20) {
+  connectToDB();
+  const skipAmount = (pageNumber - 1) * pageSize;
+  // Fetch the posts that have no parents (top-level threads...)
+  const postsQuery = Thread.find({ parentId: { $in: [null, undefined] } })
+    .sort({ createdAt: "desc" })
+    .skip(skipAmount)
+    .limit(pageSize)
+    .populate({ path: "author", model: User })
+    .populate({
+      path: "children",
+      populate: {
+        path: "author",
+        model: User,
+        select: "_id name parentId image",
+      },
+    });
+
+  const totalPostCount = await Thread.countDocuments({
+    parentId: { $in: [null, undefined] },
+  });
+
+  const posts = await postsQuery.exec();
+  const isNext = totalPostCount > skipAmount + posts.length;
+
+  return { posts, isNext };
+}
