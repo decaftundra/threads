d4bb55c65d768afad8ec48fecbe8249693442724
Julien Beghain <julienbeghain@gmail.com>
2023-12-28 18:20:50 +0100

    3:25:42 Thread details implemented

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -8,7 +8,7 @@ export default async function Home() {
   console.log(result);
   return (
     <>
-      <h1 className="head-text text-left">Home!</h1>
+      <h1 className="head-text text-left">Home</h1>
       <section className="mt-9 flex flex-col gap-10">
         {result.posts.length === 0 ? (
           <p className='no-result'>No threads found</p>
--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -96,6 +96,13 @@ const ThreadCard = ({
                   className="cursor-pointer object-contain"
                 />
               </div>
+              {isComment && comments.length > 0 && (
+                <Link href={`/thread/${id}`}>
+                  <p className="mt-1 text-subtle-medium text-gray-1">
+                    {comments.length} replies
+                  </p >
+                </Link>
+              )}
             </div>
           </div>
         </div>
